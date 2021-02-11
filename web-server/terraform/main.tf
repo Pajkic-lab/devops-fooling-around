55254a37ca19161087859156fbe1a7c35cb3d841
Pajkic-lab <markopajkic@ymail.com>
2021-02-11 14:16:59 +0100

    second commit

--- web-server/terraform/main.tf
+++ web-server/terraform/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "allow_web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["3.64.208.119"]
   }
 
   egress {
